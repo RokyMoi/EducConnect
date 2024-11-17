6ec412b7d76f9696670e52a6ba1ecf7eb502ebd5
mahir.keran <mahir.keran@edu.fit.ba>
2024-11-17 18:57:34 +0100

    Added Header for Landing page and set global font to Sour Gummy

--- frontend/EduConnect/src/app/app.component.ts
+++ frontend/EduConnect/src/app/app.component.ts
@@ -1,13 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-
+import { HeaderComponent } from './landingPage/header/header.component';
+import { HttpClient } from '@angular/common/http';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeaderComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'EduConnect';
+  http = inject(HttpClient);
+  users: any;
+
+  ngOnInit(): void {
+    console.log('AppComponent initialized');
+    this.http.get('http://localhost:8080/api/v1/users/1').subscribe(
+      (response) => {
+        this.users = response;
+        console.log('User:', this.users);
+      },
+      (error) => {
+        console.error('Error fetching user:', error);
+      }
+    );
+  }
 }
