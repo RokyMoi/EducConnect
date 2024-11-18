d333b588171a22cd2390166e90042bfedd97a996
Mahir Keran <mahir.keran@edu.fit.ba>
2024-11-18 18:59:31 +0100

    Added Landing page functionalities

--- frontend/EduConnect/src/app/landingPage/body/body.component.ts
+++ frontend/EduConnect/src/app/landingPage/body/body.component.ts
@@ -1,12 +1,18 @@
 import { Component } from '@angular/core';
-
+import {
+  Router,
+  RouterLink,
+  RouterModule,
+  RouterOutlet,
+  RouterLinkActive,
+} from '@angular/router';
 @Component({
   selector: 'app-landing-page-body',
   standalone: true,
-  imports: [],
+  imports: [RouterLink, RouterOutlet, RouterLinkActive],
   templateUrl: './body.component.html',
-  styleUrl: './body.component.css'
+  styleUrl: './body.component.css',
 })
 export class BodyComponent {
-
+  constructor(private router: Router) {}
 }
---
+++ frontend/EduConnect/src/app/signup/tutor/tutor-signup/tutor-signup.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'app-tutor-signup',
+  standalone: true,
+  imports: [RouterModule],
+  templateUrl: './tutor-signup.component.html',
+  styleUrl: './tutor-signup.component.css',
+})
+export class TutorSignupComponent {}
