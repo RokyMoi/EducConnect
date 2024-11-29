93efb8fe03d2cb6ba13ee22a2894f28e2cff3c4a
Mahir Keran <mahir.keran@edu.fit.ba>
2024-11-30 00:14:27 +0100

    Merge conflict, solve attempt

--- frontend/EduConnect/src/app/signup/Student/register-student/register-student.component.ts
+++ frontend/EduConnect/src/app/signup/Student/register-student/register-student.component.ts
@@ -1,36 +1,82 @@
 import { Component, inject, OnInit } from '@angular/core';
-import { FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import {
+  FormControl,
+  FormGroup,
+  FormsModule,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 import { AccountService } from '../../../services/account.service';
 import { NgIf } from '@angular/common';
 import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-register-student',
-  imports: [ReactiveFormsModule,NgIf],
+  standalone: true,
+  imports: [ReactiveFormsModule, NgIf],
   templateUrl: './register-student.component.html',
   styleUrl: './register-student.component.css',
-  standalone: true,
 })
-export class RegisterStudentComponent {
-  private AccountService = inject(AccountService);
-  model = {
-    firstname: '',
-    lastname: '',
-    username: '',
-    email: '',
-    password: '',
-    phoneNumber: '',
-    phoneNumberCountryCode: '',
-    countryOfOrigin: '',
-  };
-  RegistrujSe() {
-    this.AccountService.register(this.model).subscribe({
-      next: (response) => {
-        console.log(response);
-      },
-      error: (err) => {
-        console.log('Error during registration:', err);
-      },
+export class RegisterStudentComponent implements OnInit {
+  private accountService = inject(AccountService);
+  registerForm: FormGroup;
+
+  firstNameWarning = 'First name is required';
+  LastNameWarning = 'Last name is required';
+  usernameWarning = 'Username is required';
+  emailWarning = 'Email is required';
+  passwordWarning = 'Password is required';
+  PhoneWarning = 'Phone number is required';
+  PhoneCountryCodeWarning = 'Country code is required';
+  CountryWarning = 'Country of origin is required';
+
+  routerNav = inject(Router);
+  constructor() {
+    this.registerForm = new FormGroup({
+      firstName: new FormControl('', Validators.required),
+      lastName: new FormControl('', Validators.required),
+      username: new FormControl('', Validators.required),
+      email: new FormControl('', [Validators.required, Validators.email]),
+      password: new FormControl('', [
+        Validators.required,
+        Validators.minLength(8),
+        Validators.pattern(/^(?=.*[A-Z])/),
+        Validators.pattern(/^(?=.*[a-z])/),
+        Validators.pattern(/^(?=.*\d)/),
+        Validators.pattern(/^(?=.*[@$!%*?&#])/),
+      ]),
+      phoneNumber: new FormControl('', Validators.required),
+      phoneNumberCountryCode: new FormControl('', Validators.required),
+      CountryOfOrigin: new FormControl('', Validators.required),
+    });
+  }
+
+  ngOnInit(): void {}
+
+  onSubmit(): void {
+    if (this.registerForm.invalid) {
+      this.showValidationOnSubmit();
+      console.log('Form is invalid');
+    } else {
+      console.log('Form is valid, submitting...');
+      this.accountService.register(this.registerForm.value).subscribe({
+        next: (response) => {
+          console.log('Registration successful:', response);
+          this.routerNav.navigateByUrl('/student-dashboard');
+        },
+        error: (err) => {
+          console.error('Registration error:', err);
+        },
+      });
+    }
+  }
+
+  showValidationOnSubmit(): void {
+    Object.keys(this.registerForm.controls).forEach((key) => {
+      const control = this.registerForm.get(key);
+      if (control?.invalid) {
+        control.markAsTouched();
+      }
     });
   }
 }
