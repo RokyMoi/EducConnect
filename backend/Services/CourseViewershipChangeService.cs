90591f6031c3403e235ee17b5d6a4f45751edffc
Mahir Keran <mahir.keran@edu.fit.ba>
2025-05-12 00:54:03 +0200

    Further bug fixes
    Former-commit-id: 1cd9cb07e3b40ca5cffae61aeff99f78f0a3f306

--- backend/Services/CourseViewershipChangeService.cs
+++ backend/Services/CourseViewershipChangeService.cs
@@ -15,60 +15,71 @@ namespace EduConnect.Services
         private readonly IServiceScopeFactory _scopeFactory;
 
         private readonly IHubContext<CourseAnalyticsHub> _hubContext;
+        private readonly ILogger<CourseViewershipChangeService> _logger;
         private long lastSyncVersion;
 
-        public CourseViewershipChangeService(IServiceScopeFactory scopeFactory, IHubContext<CourseAnalyticsHub> hubContext)
+        public CourseViewershipChangeService(IServiceScopeFactory scopeFactory, IHubContext<CourseAnalyticsHub> hubContext, ILogger<CourseViewershipChangeService> logger)
         {
             this._scopeFactory = scopeFactory;
             this._hubContext = hubContext;
+            this._logger = logger;
         }
 
         protected override async Task ExecuteAsync(CancellationToken stoppingToken)
         {
-            Console.WriteLine("ViewershipChangeService is running.");
-
-            using var scope = _scopeFactory.CreateScope();
-            var dataContext = scope.ServiceProvider.GetRequiredService<DataContext>();
-
-            lastSyncVersion = await dataContext.Database
-            .SqlQueryRaw<long>("SELECT CHANGE_TRACKING_CURRENT_VERSION() AS Value")
-            .FirstOrDefaultAsync(cancellationToken: stoppingToken);
-
-            while (!stoppingToken.IsCancellationRequested)
+            try
             {
-                using var innerScope = _scopeFactory.CreateScope();
-                var db = innerScope.ServiceProvider.GetRequiredService<DataContext>();
+                Console.WriteLine("ViewershipChangeService is running.");
 
-                var changedCourseIds = await GetChangedCoursesIds(db, lastSyncVersion);
+                using var scope = _scopeFactory.CreateScope();
+                var dataContext = scope.ServiceProvider.GetRequiredService<DataContext>();
 
-                foreach (var courseId in changedCourseIds)
-                {
-                    var analyticsData = await db.CourseViewershipData
-                    .Where(x => x.CourseId == courseId)
-                    .GroupBy(x => 1)
-                    .Select(g => new GetAnalyticsDataResponse
-
-                    {
-                        CourseId = courseId,
-                        TotalViews = g.Count(),
-                        NumberOfUniqueVisitors = g.Select(cvd => cvd.ViewedByPersonId).Distinct().Count(),
-                        CurrentlyViewing = g.Count(cvd => cvd.EnteredDetailsAt != null && cvd.LeftDetailsAt == null),
-                        AverageViewDurationInMinutes = g
-            .Where(cvd => cvd.EnteredDetailsAt != null && cvd.LeftDetailsAt != null)
-            .Average(cvd => EF.Functions.DateDiffMinute(cvd.EnteredDetailsAt.Value, cvd.LeftDetailsAt.Value))
-                    }).FirstOrDefaultAsync();
-                    Console.WriteLine($"Updating analytics data for course {courseId}");
-                    await _hubContext.Clients.Group(courseId.ToString()).SendAsync("GetAnalyticsData", analyticsData, cancellationToken: stoppingToken);
-
-
-                }
-
-                lastSyncVersion = await db.Database
+                lastSyncVersion = await dataContext.Database
                 .SqlQueryRaw<long>("SELECT CHANGE_TRACKING_CURRENT_VERSION() AS Value")
-                .FirstAsync(cancellationToken: stoppingToken);
+                .FirstOrDefaultAsync(cancellationToken: stoppingToken);
 
-                await Task.Delay(TimeSpan.FromSeconds(10), stoppingToken);
+                while (!stoppingToken.IsCancellationRequested)
+                {
+                    using var innerScope = _scopeFactory.CreateScope();
+                    var db = innerScope.ServiceProvider.GetRequiredService<DataContext>();
+
+                    var changedCourseIds = await GetChangedCoursesIds(db, lastSyncVersion);
+
+                    foreach (var courseId in changedCourseIds)
+                    {
+                        var analyticsData = await db.CourseViewershipData
+                        .Where(x => x.CourseId == courseId)
+                        .GroupBy(x => 1)
+                        .Select(g => new GetAnalyticsDataResponse
+
+                        {
+                            CourseId = courseId,
+                            TotalViews = g.Count(),
+                            NumberOfUniqueVisitors = g.Select(cvd => cvd.ViewedByPersonId).Distinct().Count(),
+                            CurrentlyViewing = g.Count(cvd => cvd.EnteredDetailsAt != null && cvd.LeftDetailsAt == null),
+                            AverageViewDurationInMinutes = g
+                .Where(cvd => cvd.EnteredDetailsAt != null && cvd.LeftDetailsAt != null)
+                .Average(cvd => EF.Functions.DateDiffMinute(cvd.EnteredDetailsAt.Value, cvd.LeftDetailsAt.Value))
+                        }).FirstOrDefaultAsync();
+                        Console.WriteLine($"Updating analytics data for course {courseId}");
+                        await _hubContext.Clients.Group(courseId.ToString()).SendAsync("GetAnalyticsData", analyticsData, cancellationToken: stoppingToken);
+
+
+                    }
+
+                    lastSyncVersion = await db.Database
+                    .SqlQueryRaw<long>("SELECT CHANGE_TRACKING_CURRENT_VERSION() AS Value")
+                    .FirstAsync(cancellationToken: stoppingToken);
+
+                    await Task.Delay(TimeSpan.FromSeconds(10), stoppingToken);
+                }
             }
+            catch (System.Exception ex)
+            {
+
+                _logger.LogError(ex, "An error occurred in the ViewershipChangeService.");
+            }
+
 
 
         }
